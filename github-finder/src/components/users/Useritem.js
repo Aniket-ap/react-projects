b856cc852c273bc372e982b93083b6ff811aef74
Aniket-ap <aniket_pro@outlook.com>
2020-05-14 13:45:18 +0530

    github finder

---
+++ github-finder/src/components/users/Useritem.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Useritem extends Component {
+    constructor(){
+        super();
+        console.log("1234")
+    }
+    render() {
+        return (
+            <div>
+                <h1>User item</h1>
+            </div>
+        )
+    }
+}
+
+export default Useritem
