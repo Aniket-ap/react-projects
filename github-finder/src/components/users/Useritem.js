e76f6ad1573e3147e51b7db07d9cfdf2f547af08
Aniket-ap <aniket_pro@outlook.com>
2020-05-23 07:33:48 +0530

    gitFinder

--- github-finder/src/components/users/Useritem.js
+++ github-finder/src/components/users/Useritem.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export class Useritem extends Component {
-    constructor(){
-        super();
-        console.log("1234")
-    }
-    render() {
-        return (
-            <div>
-                <h1>User item</h1>
-            </div>
-        )
-    }
-}
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
 
-export default Useritem
+      <div>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
+          More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
+
+export default UserItem;
