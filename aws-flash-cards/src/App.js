43c8d05b45e4f2a2f21562c016914fe6c18b0887
Aniket-ap <aniket_pro@outlook.com>
2020-06-13 13:05:41 +0530

    aws-flash-cards

--- aws-flash-cards/src/App.js
+++ aws-flash-cards/src/App.js
@@ -1,17 +1,38 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
+import QuizBar from './components/QuizBar';
+import FlashCard from './components/FlashCard'
 
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      
-    }
+class App extends Component{
+	constructor(){
+		super();
+		this.state = {
+      cardStyle: "Random",
+      ready: false,
+		}
   }
-  render() {
+
+  userChoice = (cardStyle)=>{
+    this.setState({
+      cardStyle,
+      ready: false
+    })
+  }
+
+  nowReady = ()=>{
+    this.setState({
+      ready: true
+    })
+  }
+
+  render(){
+    console.log(this.state.cardStyle);
     return (
-      <div className="App">
-        <h1>Sanity Check</h1>
+      <div className="App align-items-center d-flex">
+        <div className="container">
+          <QuizBar userChoice={this.userChoice} />
+          <FlashCard cardStyle={this.state.cardStyle} nowReady={this.nowReady} ready={this.state.ready}  />
+        </div>
       </div>
     );
   }
