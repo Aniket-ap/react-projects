dae524a011986ce4be325a36f47bc1f347b42ba1
Aniket-ap <aniket_pro@outlook.com>
2020-06-13 12:57:00 +0530

    aws-flash-cards

--- aws-flash-cards/src/App.js
+++ aws-flash-cards/src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      
+    }
+  }
   render() {
     return (
       <div className="App">
