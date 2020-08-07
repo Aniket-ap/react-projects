4e01ba9bbd247f182206966d374ed4632f032e66
Aniket Pradhan <aniket_pro@outlook.com>
2020-08-07 20:45:00 +0530

    added logic for buy now and remove cart

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "react-toastify/dist/React-Toastify.css"
 import './App.css';
+import { toast } from 'react-toastify';
 
 function App() {
+
+  const [cartItem, setCartItem] = useState([])
+
+  const addInCart = item => {
+    const isAlreadyAdded = cartItem.findIndex(function(array){
+      return array.id === item.id
+    })
+
+    if(isAlreadyAdded !== -1) {
+      toast("Already Added In Cart", {
+        type : "error"
+      })
+    }
+
+    setCartItem([...cartItem, item])
+  }
+
+  const buyNow = () => {
+    setCartItem([])
+
+    toast("Purchase Complete", {
+      type : "success"
+    })
+  }
+
+  const removeItem = (item) => {
+    setCartItem(cartItem.filter(singleItem => singleItem.id !== item.id))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
