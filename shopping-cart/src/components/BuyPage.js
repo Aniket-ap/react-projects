7c0d633a294cc429bdb251f73824785493a4abb0
Aniket Pradhan <aniket_pro@outlook.com>
2020-08-08 12:18:43 +0530

    -a

--- shopping-cart/src/components/BuyPage.js
+++ shopping-cart/src/components/BuyPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import { Axios } from "axios";
+import Axios  from "axios";
 
 import { random, commerce } from "faker";
 
@@ -13,13 +13,34 @@ const BuyPage = ({ addInCart }) => {
 
   const fetchPhotos = async () => {
     const { data } = await Axios.get(url);
-  };
 
-  const { photos } = data;
+    const { photos } = data;
+
+    const allProduct = photos.map((photo) => ({
+      smallImage: photo.src.medium,
+      tinyImage: photo.src.tiny,
+      productName: random.word(),
+      productPrice: commerce.price(),
+      id: random.uuid(),
+    }));
+
+    setProduct(allProduct)
+  };
 
   useEffect(() => {
     fetchPhotos();
   }, []);
+
+  return(
+    <Container fluid>
+      <h1 className="text-success text-center">Buy Page</h1>
+      <Row>
+        {product.map(product => (
+          <span key={product.id}>{product.productName}</span>
+        ))}
+      </Row>
+    </Container>
+  )
 };
 
 export default BuyPage;
