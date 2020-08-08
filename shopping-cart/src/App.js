49d4be6c5c42062c00c2ddb513d6c10e22879333
Aniket Pradhan <aniket_pro@outlook.com>
2020-08-08 12:18:15 +0530

    -a

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import "react-toastify/dist/React-Toastify.css"
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import { toast } from 'react-toastify';
+import BuyPage from './components/BuyPage';
 
 function App() {
 
@@ -36,20 +37,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BuyPage addInCart={addInCart} />
     </div>
   );
 }
